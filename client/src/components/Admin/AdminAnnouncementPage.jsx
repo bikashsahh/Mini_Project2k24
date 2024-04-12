f7c2c77c706296c58ac4e2b490b3b07318cbe01c
bikashsahh <bikashsah0312@gmail.com>
2024-04-13 02:52:42 +0530

    adminAnnouncementPage is being updated

--- client/src/components/Admin/AdminAnnouncementPage.jsx
+++ client/src/components/Admin/AdminAnnouncementPage.jsx
@@ -6,6 +6,7 @@ import {
   TextField,
   Button,
   CircularProgress,
+  Divider,
 } from "@mui/material";
 import axios from "axios";
 
@@ -18,13 +19,11 @@ const AdminAnnouncementPage = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     setIsLoading(true);
-
     try {
       const formData = new FormData();
       formData.append("title", title);
       formData.append("description", description);
       formData.append("file", file);
-
       const response = await axios.post(
         "http://localhost:3000/announcements",
         formData,
@@ -34,9 +33,7 @@ const AdminAnnouncementPage = () => {
           },
         }
       );
-
       console.log("Server response:", response.data);
-      // Reset the form after successful submission
       setTitle("");
       setDescription("");
       setFile(null);
@@ -52,39 +49,79 @@ const AdminAnnouncementPage = () => {
   };
 
   return (
-    <Box>
-      <Typography variant="h5" gutterBottom>
-        Announcement Management
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        // bgcolor: "#E3F2FD",
+        justifyContent: "center",
+        p: 2,
+        mt: "10%",
+      }}
+    >
+      <Typography
+        variant="h2"
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          fontSize: "3rem",
+          fontWeight: "bold",
+          padding: "0.5rem 1rem",
+          mb: 3,
+        }}
+      >
+        Make a Announcement
       </Typography>
-      <form onSubmit={handleSubmit}>
-        <Stack spacing={2}>
-          <TextField
-            label="Title"
-            variant="outlined"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-            required
-          />
-          <TextField
-            label="Description"
-            variant="outlined"
-            multiline
-            rows={4}
-            value={description}
-            onChange={(e) => setDescription(e.target.value)}
-            required
-          />
-          <input type="file" onChange={handleFileChange} required />
-          <Button
-            variant="contained"
-            color="primary"
-            type="submit"
-            disabled={isLoading}
-          >
-            {isLoading ? <CircularProgress size={24} /> : "Create Announcement"}
-          </Button>
-        </Stack>
-      </form>
+      <Box
+        sx={{
+          bgcolor: "#EDE7F6",
+          color: "",
+          p: 2,
+          borderRadius: "20px",
+          mb: 2,
+          width: "90%",
+        }}
+      >
+        <form onSubmit={handleSubmit}>
+          <Stack spacing={2}>
+            <TextField
+              label="Title"
+              variant="outlined"
+              color="secondary"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              required
+              fullWidth
+            />
+            <TextField
+              label="Description"
+              variant="outlined"
+              color="secondary"
+              multiline
+              rows={4}
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+              required
+              fullWidth
+            />
+            <input type="file" onChange={handleFileChange} required />
+            <Button
+              variant="contained"
+              color="secondary"
+              type="submit"
+              disabled={isLoading}
+              fullWidth
+            >
+              {isLoading ? (
+                <CircularProgress size={24} />
+              ) : (
+                "Create Announcement"
+              )}
+            </Button>
+          </Stack>
+        </form>
+      </Box>
     </Box>
   );
 };
